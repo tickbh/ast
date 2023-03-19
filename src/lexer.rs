f60efd3c57f8b4a4a4fe4c06175e3f7eba7522d4
ticbh <dwhat@vip.qq.com>
2023-03-19 20:12:38 +0800

    add fix

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,30 +1,46 @@
+use std::{ops::BitAnd, sync::Arc};
 use regex::Regex;
+use std::fmt::Debug;
 
-
+#[derive(Clone, Eq, PartialEq, Hash)]
 pub struct LexToken {
-    pub ty: String,
-    pub value: String,
+    pub ty: &'static str,
+    pub value: Arc<String>,
     pub lineno: usize,
-    pub pos: usize,
+    pub start: usize,
+    pub end: usize,
 }
 
+impl Debug for LexToken {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let val = self.value.get(self.start..self.end).unwrap();
+        f.debug_struct("LexToken").field("ty", &self.ty).field("value", &val).field("lineno", &self.lineno).field("start", &self.start).field("end", &self.end).finish()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct LexRegex {
+    pub re: Regex,
+    pub ty: &'static str,
+}
+
+#[derive(Clone, Debug)]
 pub struct Lexer {
-    pub res: Vec<Regex>,
-    pub data: String,
+    pub res: Vec<LexRegex>,
+    pub data: Arc<String>,
     pub pos: usize,
     pub len: usize,
-
-    pub ingore: String,
+    pub ignore: String,
 }
 
 impl Default for Lexer {
     fn default() -> Self { 
         Lexer {
             res: vec![],
-            data: String::new(),
+            data: Arc::new(String::new()),
             pos: 0,
             len: 0,
-            ingore: " \t".to_string(),
+            ignore: " \t".to_string(),
         }
      }
 }
@@ -32,11 +48,78 @@ impl Default for Lexer {
 impl Lexer {
 
     pub fn new(data: String) -> Lexer {
-        Lexer {  len: data.len(), data, ..Default::default() }
+        Lexer {  len: data.len(), data: Arc::new(data), ..Default::default() }
     }
 
-    pub fn get_token(&mut self) -> () {
+    pub fn add_regex(&mut self, ty: &'static str, re: Regex) {
+        let reg = LexRegex {
+            ty, re
+        };
+        self.res.push(reg);
+    }
 
+    pub fn get_next_pos(&self, ori: usize) -> Option<usize> {
+        let bytes = self.data.as_bytes();
+        if ori >= bytes.len() {
+            return None;
+        }
+        let mut byte = bytes[ori];
+        let mut byte_len = 0;
+        loop {
+            println!("byte = {} byte_len = {}", byte, byte_len);
+            if byte.bitand(0x80) == 0 {
+                println!("byte = {} break", byte);
+                break;
+            }
+            byte_len += 1;
+            byte = byte.checked_shl(1).unwrap_or(0);
+        }
+        byte_len = byte_len.max(1);
+
+        let pos = ori + byte_len;
+        if pos > bytes.len() {
+            None
+        } else {
+            Some(pos)
+        }
+    }
+
+    pub fn get_token(&mut self) -> Option<LexToken> {
+        let mut ori = self.pos;
+        loop {
+            let pos = self.get_next_pos(ori);
+            println!("ori = {} pos = {:?}", ori, pos);
+            if pos.is_none() {
+                return None;
+            }
+            if self.ignore.contains(self.data.get(ori .. pos.unwrap()).unwrap()) {
+                self.pos = pos.unwrap();
+                ori = pos.unwrap();
+                continue;
+            }
+
+            for re in &self.res {
+
+                println!("regex ori = {} pos = {:?}", ori, pos);
+
+                if let Some(p) = re.re.find_at(&self.data, ori) {
+                    if p.start() != ori {
+                        continue;
+                    }
+                    self.pos = p.end();
+                    return Some(LexToken {
+                        ty: re.ty,
+                        value: self.data.clone(),
+                        lineno: 0,
+                        start: p.start(),
+                        end: p.end(),
+                    })
+                }
+            }
+            println!("now data = {:?}", self.data.get(ori .. pos.unwrap()));
+            println!("ori = {:?} pos = {:?}", ori, pos);
+            ori = pos.unwrap();
+        }
     }
 
     pub fn parser(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,87 @@
 extern crate lang_ast;
 
 use lang_ast::Lexer;
+use regex::Regex;
 fn main() {
-    let lex = Lexer::new("1+1".to_string());
+    // let value = "\"1+1\"我是中".to_string();
+    // println!("string size = {}", value.len());
+
+    // let value = "right11=22".to_string();
+
+    // let rex = Regex::new(r"[A-Za-z_][A-Za-z0-9_]*").unwrap();
+    // match rex.find_at(&value, 0) {
+    //     Some(pos) => {
+    //         println!("text = {:?}", value.get(0..pos.end()));
+    //     }
+    //     None => {
+
+    //     }
+    // } ;
+    // let a = "\"([^\\\n]|(\\.))*?\"";
+
+    // let value = "\"righ\\\"t11=\"22".to_string();
+    // let rex = Regex::new(r#""([^\\\n]|(\\.))*?""#).unwrap();
+    // match rex.find_at(&value, 0) {
+    //     Some(pos) => {
+    //         println!("text = {:?}", value.get(0..pos.end()));
+    //     }
+    //     None => {
+    //         println!("not found string");
+    //     }
+    // } ;
+
+    // let value = r###"""" aaa fdsafdsa  """"###.to_string();
+    // let value = "\"\"\"aaa   
+    // fdsafdsa  \"\"\"".to_string();
+    // println!("aaaavalues = {:?}!!!!!!!", value.as_bytes());
+    // let rex = Regex::new(r#""""([^\n]|[^\r]|(\.))*?""""#).unwrap();
+    // match rex.find_at(&value, 0) {
+    //     Some(pos) => {
+    //         println!("text = {:?} end = {}", value.get(0..pos.end()), pos.end());
+    //     }
+    //     None => {
+
+    //         println!("not found string");
+    //     }
+    // } ;
+
+
+    // let value = "22887798989798789789789uL".to_string();
+    // let rex = Regex::new(r#"\d+([uU]|[lL]|[uU][lL]|[lL][uU])?"#).unwrap();
+    // match rex.find_at(&value, 0) {
+    //     Some(pos) => {
+    //         println!("text = {:?}", value.get(0..pos.end()));
+    //     }
+    //     None => {
+    //         println!("not found string");
+    //     }
+    // } ;
+    // return;
+// t_STRING = r'\"([^\\\n]|(\\.))*?\"'
+// # Identifiers
+// t_ID = r'[A-Za-z_][A-Za-z0-9_]*'
+
+// # Integer literal
+// t_INTEGER = r'\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
+
+// # Floating literal
+// t_FLOAT = r'((\d+)(\.\d+)(e(\+|-)?(\d+))? | SB(\d+)e(\+|-)?(\d+))([lL]|[fF])?'
+
+// # String literal
+// t_STRING = r'\"([^\\\n]|(\\.))*?\"'
+    let value = "right11=2211;\naaass=222uiii".to_string();
+    let xx = Regex::new(r"[A-Za-z_][A-Za-z0-9_]*").unwrap();
+    println!("======={:?}", xx.shortest_match_at(&value, 5));
+
+    let mut lex = Lexer::new(value);
+    lex.add_regex("id", Regex::new(r"[A-Za-z_][A-Za-z0-9_]*").unwrap());
+    lex.add_regex("equal", Regex::new(r"=").unwrap());
+    lex.add_regex("end", Regex::new(r";").unwrap());
+    lex.add_regex("line", Regex::new(r"\n").unwrap());
+    lex.add_regex("num", Regex::new(r"\d+([uU]|[lL]|[uU][lL]|[lL][uU])?").unwrap());
+
+    while let Some(token) = lex.get_token() {
+        println!("token = {:?}", token);
+    }
     println!("Hello, world!");
 }
